10227e4a698be5c4e40e7dfbe3702bee100a9396
BOTXavier <xavier.dartevel@gmail.com>
2024-09-04 14:17:58 +1200

    update with loops and prints

--- zChanger_Prusa.py
+++ zChanger_Prusa.py
@@ -1,51 +1,28 @@
+input_file = 'pdms_gradient.gcode'              # Name of the gfile you want to modify
+output_file = 'Modified_' + input_file     # name of the gfile you want to get after the process.
+
+
 # DEfine the patterns of layer's height
 layer_height_pattern = {
-    0: 0.050,
-    1: 0.050,
-    2: 0.050,
-    3: 0.050,
-    4: 0.050,
-    5: 0.050,
-    6: 0.050,
-    7: 0.050,
-    8: 0.050,
-    9: 0.050,
-    10: 0.100,
-    11: 0.100,
-    12: 0.100,
-    13: 0.100,
-    14: 0.100,
-    15: 0.150,
-    16: 0.150,
-    17: 0.150
-    #4: 0.400
+
 }
 
+
+#good combos : 0.05/85 ; 0.100/130 ; 0.150/185
 layer_flow_pattern = {
-    0: 100,
-    1: 100,
-    2: 100,
-    3: 100,
-    4: 100,
-    5: 100,
-    6: 100,
-    7: 100,
-    8: 100,
-    9: 100,
-    10: 125,
-    11: 125,
-    12: 125,
-    13: 125,
-    14: 125,
-    15: 180,
-    16: 180,
-    17: 180
+
 }
 
+for i in range(140):
+    if i < 100: 
+        layer_height_pattern[i] = 0.05
+        layer_flow_pattern[i] = 85
+    else:
+        layer_height_pattern[i] = 0.150
+        layer_flow_pattern[i] = 185
 
-input_file = 'test_05_200.gcode'              # Name of the gfile you want to modify
-output_file = 'Modified_2' + input_file     # name of the gfile you want to get after the process.
 
+#print(layer_height_pattern)
 max_nb_pattern = len(layer_height_pattern)
 
 
@@ -94,43 +71,48 @@ def modify_gcode_line(line, new_value,flowrate_change_on=False):
 def write_new_code(output_file, lines,flowrate_change_on=False):
     current_layer = 0 
     New_Z_Value = 0 #extract_z_origin_value(lines)
-    previous_current_Z = 0
-    previous_Z = 0
-    previous_layer_originZ = 0
-    New_layer = False
+    previous_current_Z = 0 # Z of this layer before the code was modified
+    previous_Z = 0 # Z before the code was modified
+    previous_layer_originZ = 0 # Z of the layer before the one we are currently, from before the modifications
+    New_layer = False # have we entered a new layer or not 
 
+    if  flowrate_change_on==True: ## Check if the two patterns are matching
+            if max_nb_pattern != len(layer_flow_pattern):
+                print("Error : Height and flow patterns are not in the same number")
     
-    with open(output_file, 'w') as out_file:
-        for line in lines:
+    with open(output_file, 'w') as out_file: #Open the writing file 
+        for line in lines: 
 
-            if  flowrate_change_on==True:
-                if max_nb_pattern != len(layer_flow_pattern):
-                    print("Error : Height and flow patterns are not in the same number")
-
-
-
-            if line.startswith("; "):
+            if line.startswith("; "): # then those are comments, no need to modify them, nor even to write them
                 out_file.write(line)
 
-            elif line.startswith("M"):
+            elif line.startswith("M"): # Then those are parameters that we don't want to change neither
                 out_file.write(line)
-
-            
 
             else:
             
-                if 'Z' in line:
+                if 'Z' in line: # we gotta modify it
                     if New_layer : #If we just changed the layer, we keep the previous value 
                         previous_layer_originZ = previous_Z
-                        previous_Z = float(line.split(':')[1])
-                        if flowrate_change_on:
-                            out_file.write( f"{'M221 S'}{layer_flow_pattern[(current_layer) % max_nb_pattern]}" + '\n')
-                        New_layer = False
+                        previous_Z = float(line.split(':')[1]) # we read the comment line stating the current Z
 
-                    parts = line.split()
-                    for i, part in enumerate(parts):
+                        if flowrate_change_on:
+                            new_flow_rate = layer_flow_pattern[(current_layer) % max_nb_pattern]
+                            out_file.write( f"{';Layer number:'}{current_layer}" + '\n')  # Add a comment
+                            out_file.write( f"{';New Layer Height:'}{layer_height_pattern[(current_layer) % max_nb_pattern]}" + '\n')  # add a comment
+                            out_file.write( f"{';New Z:'}{New_Z_Value}" + '\n') # add a comment 
+                            out_file.write( f"{'M221 S'}{new_flow_rate}" + '\n') # change the flowrate for this new layer
+#                            out_file.write( f"{'G4 P'}{500}" + '\n') # This line adds a 500µs break 
+
+                            
+                        New_layer = False # we entered the new layer
+
+                    parts = line.split() # we want to find the part of the line where we have to modify the Z
+
+                    for part in parts:
                         if part.startswith("Z"):
-                            Z = float(part.split('Z')[1])
+                            Z = float(part.split('Z')[1]) # we read what Z is written to know what modification to do
+                            break
                     
                     if Z == previous_layer_originZ:
                         modified_line = modify_gcode_line(line,previous_current_Z)
@@ -139,21 +121,19 @@ def write_new_code(output_file, lines,flowrate_change_on=False):
                         modified_line = modify_gcode_line(line,New_Z_Value)
                         out_file.write(modified_line + '\n')
                     else:
-                        modified_line = modify_gcode_line(line,New_Z_Value + 0.122)    
-                        out_file.write(modified_line + '\n')
-
-                
+                        modified_line = modify_gcode_line(line,New_Z_Value + 0.122) #0.122 chosed arbitrarily after reading the code   
+                        out_file.write(modified_line + '\n') 
                     
                 else: 
                     out_file.write(line)
-                    
 
                 
 ## if we find a new layer, we increase the current layer number 
             if line.startswith(';LAYER_CHANGE'): 
                 current_layer += 1
                 previous_current_Z = New_Z_Value
-                New_Z_Value += layer_height_pattern[(current_layer) % max_nb_pattern]
+                new_height = layer_height_pattern[(current_layer) % max_nb_pattern]
+                New_Z_Value += new_height
                 New_layer = True
 
 
